85f676474007b41196e8fe48e5aeb5f78a678096
pptnz <eye2988@gmail.com>
2017-11-17 02:43:57 +0900

    Modify urls.py for photo

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -3,5 +3,12 @@ import user.views as views
 
 urlpatterns = [
     url(r'^signin$', views.sign_in, name='signin'),
-    url(r'^signup$', views.sign_up, name='signup')
+    url(r'^signup$', views.sign_up, name='signup'),
+
+    url(r'^photo', views.photo_post, name='photo_post'),
+    url(r'^photo/random', views.photo_list_random, name='photo_list_random'),
+    url(r'^photo/location', views.photo_list_location, name='photo_list_location'),
+    url(r'^photo/:tag', views.photo_list_tag, name='photo_list_tag'),
+    url(r'^photo/:id', views.photo_detail, name='photo_list_detail'),
+
 ]
