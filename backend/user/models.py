6661132bb0f6ce1b61dd43eb1a00e95c0dd9ef6d
pptnz <eye2988@gmail.com>
2017-11-17 02:34:31 +0900

    Add Tag, Image models

--- backend/user/models.py
+++ backend/user/models.py
@@ -36,3 +36,48 @@ class User(models.Model):
     # TODO: Implement methods after adding Level field
     # def set_level(self):
     #     # level-up logic
+
+
+class Tag(models.Model):
+    content = models.CharField(max_length=5)
+
+    planes = models.ManyToManyField('Plane')
+    replies = models.ManyToManyField('Reply')
+    photos = models.ManyToManyField(Photo, related_name='photos')
+
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='uploads/%Y/%m/%d')
+
+    author = models.ForeignKey(User)
+    # TODO: Add color, location
+    is_reported = models.BooleanField()
+    date = models.DateTimeField()
+
+    # descending order of date
+    def get_by_date(self):
+        return Photo.objects.all().order_by('-date')
+
+    # random order
+    def get_randomly(self):
+        return Photo.objects.all().order_by('?')
+
+    # get all photos with specific tag
+    def get_by_tag(self, tag):
+        return list(tag.photos.all().values())
+
+    # TODO: Add get by color, location
+    # def get_by_color:
+    # def get_by_location:
+
+    # get report by user and change the flag
+    def report_bad_content(self):
+        self.is_reported = True
+
+    # delete the photo
+    def delete(self, *args, **kwargs):
+        # delete the image file
+        self.image.delete()
+
+        # delete the Photo instance
+        super(Photo, self).delete(*args, **kwargs)
