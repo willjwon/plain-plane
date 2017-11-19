fd72456f6653fdca2cef29d393ed5eb20378f6f6
pptnz <eye2988@gmail.com>
2017-11-19 13:20:21 +0900

    Implement backend for photo_list, photo_list_color, photo_detail

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -5,10 +5,7 @@ urlpatterns = [
     url(r'^signin$', views.sign_in, name='signin'),
     url(r'^signup$', views.sign_up, name='signup'),
 
-    url(r'^photo', views.photo_post, name='photo_post'),
-    url(r'^photo/random', views.photo_list_random, name='photo_list_random'),
-    url(r'^photo/location', views.photo_list_location, name='photo_list_location'),
-    url(r'^photo/:tag', views.photo_list_tag, name='photo_list_tag'),
-    url(r'^photo/:id', views.photo_detail, name='photo_list_detail'),
-
+    url(r'^photo$', views.photo_list, name='photo_list'),
+    url(r'^photo/(?P<photo_color>(RED|ORANGE|YELLOW|GREEN|BLUE|INDIGO|VIOLET|BLACK))$', views.photo_list_color, name='photo_list_color'),
+    url(r'^photo/(?P<photo_id>[0-9]+)$', views.photo_detail, name='photo_detail'),
 ]
