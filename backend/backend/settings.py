6db7d9ef0ab93180414c6e47af643fbe46d10443
pptnz <eye2988@gmail.com>
2017-11-20 13:48:30 +0900

    Change the directory to store photos

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,5 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'uploaded_images')
+MEDIA_ROOT = os.path.join(BASE_DIR, '.', 'uploaded_images')
 MEDIA_URL = '/images/'
