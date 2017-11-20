d6cbede6c81252b211cf41ceaa81305a1dc7454f
pptnz <eye2988@gmail.com>
2017-11-20 13:20:50 +0900

    Rename PhotoListViewSet to PhotoViewSet

--- backend/gallery/urls.py
+++ backend/gallery/urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from .viewsets import PhotoListViewSet
+from .viewsets import PhotoViewSet
 
 # initiate router and register all endpoints
 router = routers.DefaultRouter()
-router.register('', PhotoListViewSet, 'photo_list')
+router.register('', PhotoViewSet, 'photo')
 
 # Wire up our API with our urls
 urlpatterns = [
     url(r'', include(router.urls)),
 ]
-# urlpatterns = [
-#     url(r'^$', views.photo_list, name='photo_list'),
-#     url(r'^color/(?P<color_id>[0-7])$', views.photo_list_color, name='photo_list_color'),
-#     url(r'^(?P<photo_id>[0-9]+)$', views.photo_detail, name='photo_detail'),
-# ]
--- backend/gallery/viewsets.py
+++ backend/gallery/viewsets.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, filters, status
+from rest_framework import viewsets, status
 from rest_framework.decorators import list_route, detail_route
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
@@ -6,7 +6,7 @@ from .serializers import PhotoSerializer
 from .models import Photo
 
 
-class PhotoListViewSet(viewsets.ModelViewSet):
+class PhotoViewSet(viewsets.ModelViewSet):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
 
@@ -29,7 +29,6 @@ class PhotoListViewSet(viewsets.ModelViewSet):
         photo = get_object_or_404(queryset, pk=pk)
         photo.is_reported = True
         photo.save()
-        serializer = PhotoSerializer(photo)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
