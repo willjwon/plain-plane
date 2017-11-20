4be389fb1d0233e905e3cba6cdb9ac9b91d2d440
pptnz <eye2988@gmail.com>
2017-11-20 18:15:04 +0900

    Remove enum field

--- backend/gallery/models.py
+++ backend/gallery/models.py
@@ -1,8 +1,8 @@
 import uuid
 
 from django.db import models
-from django_enumfield import enum
 # Create your models here.
+
 
 class Tag(models.Model):
     content = models.CharField(max_length=5)
@@ -12,30 +12,26 @@ class Tag(models.Model):
     photos = models.ForeignKey('Photo', related_name='photos')
 
 
-class Color(enum.Enum):
-    RED = 0
-    ORANGE = 1
-    YELLOW = 2
-    GREEN = 3
-    BLUE = 4
-    INDIGO = 5
-    VIOLET = 6
-    BLACK = 7
-
-
-def unique_filename(instance, filename):
-    extension = filename.split(".")[-1]
-    return "{}.{}".format(uuid.uuid4(), extension)
-
-
 class Photo(models.Model):
     # uploaded image
+    def unique_filename(instance, filename):
+        extension = filename.split(".")[-1]
+        return "{}.{}".format(uuid.uuid4(), extension)
 
     image = models.ImageField('Image', upload_to=unique_filename)
 
     # author = models.ForeignKey(User)
     is_reported = models.BooleanField()
-    color = enum.EnumField(Color)
+
+    # RED = 0
+    # ORANGE = 1
+    # YELLOW = 2
+    # GREEN = 3
+    # BLUE = 4
+    # INDIGO = 5
+    # VIOLET = 6
+    # BLACK = 7
+    color = models.IntegerField()
 
     # delete the photo
     def delete(self, *args, **kwargs):
