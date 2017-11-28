a6a80189f4aee819633c9e9f8e860994790c3171
pptnz <eye2988@gmail.com>
2017-11-29 08:20:54 +0900

    Generate components: photo-detail, photo-list, photo-post

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoDetailComponent } from './photo-detail/photo-detail.component';
+import { PhotoPostComponent } from './photo-post/photo-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoListComponent,
+    PhotoDetailComponent,
+    PhotoPostComponent
   ],
   imports: [
     BrowserModule
