b68a524de6c1afef929491d4966a80deb9a0d97c
Jonghoon Won <baryberri@icloud.com>
2017-11-29 14:19:28 +0900

    Generate MainPageComponent and AppRoutingModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,21 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { CookieXSRFStrategy, HttpModule, XSRFStrategy } from '@angular/http';
 
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoDetailComponent } from './photo-detail/photo-detail.component';
 import { PhotoPostComponent } from './photo-post/photo-post.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PhotoListComponent,
     PhotoDetailComponent,
-    PhotoPostComponent
+    PhotoPostComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    CommonModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [
     {
