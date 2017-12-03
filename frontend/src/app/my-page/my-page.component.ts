cd26f90e93b356cfd8ead8d5492479c2c3fd8adf
Jonghoon Won <baryberri@icloud.com>
2017-12-04 04:49:20 +0900

    Finish implementing main page

--- frontend/src/app/my-page/my-page.component.ts
+++ frontend/src/app/my-page/my-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../models/user.service';
+import { ReplyService } from '../models/reply.service';
+import { User } from '../models/user';
+import { Reply } from '../models/reply';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-page',
@@ -7,9 +12,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyPageComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  replies: Reply[] = [];
+  pageIndex = 0;
+
+  constructor(private userService: UserService,
+              private replyService: ReplyService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.getSignedInUser();
   }
 
+  getSignedInUser() {
+    this.userService.getUser().then(user => {
+      this.user = user;
+      this.getReplies();
+    });
+  }
+
+  getReplies() {
+    this.replyService.getReplyByUser(this.user.user_id).then(replies => {
+      this.replies = replies;
+    });
+  }
+
+  checkReply(num: number): boolean {
+    return num < this.replies.length;
+  }
+
+  onClickReply(reply: Reply) {
+    this.router.navigate([`/reply/${reply.reply_id}`]);
+  }
+
+  onClickPageDown() {
+    if (this.pageIndex > 0) {
+      this.pageIndex -= 1;
+    }
+  }
+
+  onClickPageUp() {
+    const maxPageIndex = Math.ceil(this.replies.length / 6) - 1;
+    if (this.pageIndex < maxPageIndex) {
+      this.pageIndex += 1;
+    }
+  }
 }
