9dff95e4fa7570d8eefc62e7bf508039274affe5
pptnz <eye2988@gmail.com>
2017-12-19 02:31:55 +0900

    Test for ngOnInit in reply component

--- frontend/src/app/reply/reply.component.init.spec.ts
+++ frontend/src/app/reply/reply.component.init.spec.ts
@@ -32,12 +32,28 @@ describe('ReplyComponent', () => {
 
 
   beforeEach((done: any) => {
-    expectedReply = REPLIES[0];
-    expectedPlane = PLANES[0];
-    expectedUser = USERS[0];
+    expectedReply = {
+      reply_id: 1,
+      plane_author: 1,
+      reply_author: 2,
+      original_content: 'xxx',
+      original_tag: 'study',
+      content: 'aaa',
+      is_reported: false,
+      level: 'Plain'
+    };
+    expectedPlane = {author_id: 1, plane_id: 0, content: 'aaa', tag: 'study', level: 'Plain'};
+    expectedUser = {
+      user_id: 1,
+      username: 'aa',
+      level: 'Plain',
+      today_reply_count: 3,
+      today_write_count: 3,
+      total_likes: 1
+    };
 
     activatedRoute = new ActivatedRouteStub();
-    activatedRoute.testParamMap = {id: expectedReply.reply_id};
+    activatedRoute.testParamMap = {id: expectedPlane.plane_id};
 
     router = jasmine.createSpyObj('router', ['navigate']);
 
@@ -50,17 +66,18 @@ describe('ReplyComponent', () => {
     userServiceSpy = jasmine.createSpyObj('UserService', ['getUser']);
     userServiceSpy.getUser.and.returnValue(Promise.resolve(expectedUser));
 
-    replyServiceSpy = jasmine.createSpyObj('ReplyService', ['getReply', 'report']);
+    replyServiceSpy = jasmine.createSpyObj('ReplyService', ['getReply']);
     replyServiceSpy.getReply.and.returnValue(Promise.resolve(expectedReply));
-    replyServiceSpy.report.and.returnValue(Promise.resolve(expectedReply));
 
     comp = new ReplyComponent(router, <any> activatedRoute, locationSpy, planeServiceSpy, userServiceSpy, replyServiceSpy);
     comp.ngOnInit();
 
     // OnInit calls HDS.getReply; wait for it to get the fake reply
-    replyServiceSpy.getReply.calls.first().returnValue.then(done);
+    userServiceSpy.getUser.calls.first().returnValue.then(done);
   });
   it('should be created', () => {
     expect(comp).toBeTruthy();
   });
 });
+
+
--- frontend/src/testing/models/fake-user.service.ts
+++ frontend/src/testing/models/fake-user.service.ts
@@ -6,7 +6,7 @@ import { UserService } from '../../app/models/user.service';
 
 export const USERS: User[] = [
   { user_id: 1, username: 'aa', level: 'Plain', today_reply_count: 3, today_write_count: 3, total_likes: 1 },
-  { user_id: 2, username: 'bb', level: 'Plain', today_reply_count: 0, today_write_count: 3, total_likes: 1 },
+  { user_id: 2, username: 'bb', level: 'Plain', today_reply_count: 3, today_write_count: 3, total_likes: 1 },
 ];
 
 export class FakeUserService {
