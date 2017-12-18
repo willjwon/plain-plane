4ee8236e88cbb21d1504e02427d0a3d7451a030b
Jonghoon Won <baryberri@icloud.com>
2017-12-18 13:16:01 +0900

    Complete user model testing

--- backend/user/tests.py
+++ backend/user/tests.py
@@ -12,8 +12,14 @@ class UserTest(TestCase):
         user = user_model.User(username="testusername", password="testpassword")
         user.save()
 
-        level = Level(flavor='flavor', max_today_reply=1, max_today_write=2, next_level_likes=3, plane_life_span=4)
+        level = Level(flavor='Plain', max_today_reply=1, max_today_write=2, next_level_likes=3, plane_life_span=4)
         level.save()
+
+        next_level = Level(flavor='Strawberry', max_today_reply=1, max_today_write=2, next_level_likes=20, plane_life_span=4)
+        next_level.save()
+
+        soy_sauce_level = Level(flavor='SoySauce', max_today_reply=1, max_today_write=2, next_level_likes=3, plane_life_span=4)
+        soy_sauce_level.save()
 
         self.user = User(user=user, today_write_count=10, today_reply_count=10, total_likes=10, level=level)
         self.user.save()
@@ -103,6 +109,24 @@ class UserTest(TestCase):
         result = PasswordResetTokenGenerator()._make_hash_value(self.user.user, 0)
         self.assertIsNotNone(result)
 
+    def test_initialize_today_write(self):
+        self.user.initialize_today_write()
+        self.user.save()
+        self.assertEqual(self.user.today_write_count, 2)
 
+    def test_initialize_today_reply(self):
+        self.user.initialize_today_reply()
+        self.user.save()
+        self.assertEqual(self.user.today_reply_count, 1)
+
+    def test_set_level_soy_sauce(self):
+        self.user.total_likes = -20
+        self.user.set_level()
+        self.assertEqual(self.user.level.flavor, "SoySauce")
+
+    def test_set_level_up(self):
+        self.user.total_likes = 10
+        self.user.set_level()
+        self.assertEqual(self.user.level.flavor, "Strawberry")
 
 
