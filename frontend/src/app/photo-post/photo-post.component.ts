522a10bfe9674d6a38926714a3a95887ac957757
pptnz <eye2988@gmail.com>
2017-12-18 16:01:50 +0900

    Add allowable image extensions

--- frontend/src/app/photo-post/photo-post.component.ts
+++ frontend/src/app/photo-post/photo-post.component.ts
@@ -13,21 +13,27 @@ export class PhotoPostComponent implements OnInit {
   ngOnInit() {
   }
 
+  image_extension = ['jpg', 'jpeg', 'gif', 'png', 'apng', 'svg', 'bmp',
+                     'JPG', 'JPEG', 'GIF', 'PNG', 'APNG', 'SVG', 'BMP', ]
+
   private upload() {
     const fileBrowser = this.fileInput.nativeElement;
     if (fileBrowser.files && fileBrowser.files[0]) {
       let fileSize = fileBrowser.files[0].size;
       let fileType = fileBrowser.files[0].name.substring(fileBrowser.files[0].name.length - 3);
-      if (fileSize >= 2097152) {
-        //some action
-        console.log("size limit");
-        return;
-      }
-      if (fileType != "jpg") {
-        //some action
+
+      if (this.image_extension.indexOf(fileType) == -1) {
+        alert('The file is not an image');
         console.log("type limit");
         return;
       }
+
+      if (fileSize >= 2097152) {
+        alert('Image size exceeds 2MB');
+        console.log("size limit");
+        return;
+      }
+
       const formData = new FormData();
       formData.append('author_id', sessionStorage.getItem('user_id'));
       formData.append('image', fileBrowser.files[0]);
