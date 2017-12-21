283ca9539ee2dd388bc0362f2877aa61969bb2cd
pptnz <eye2988@gmail.com>
2017-12-21 16:02:11 +0900

    Modify serializer in backend gallery

--- backend/gallery/serializer.py
+++ backend/gallery/serializer.py
@@ -6,15 +6,4 @@ from .color_picker import *
 class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
-        fields = ('id', 'author', 'image', 'tag', 'color')
-
-    def create(self, validated_data):
-        author = validated_data['author']
-        image = validated_data['image']
-        color = get_color(image)
-        tag = validated_data['tag']
-
-        photo = Photo(author=author, image=image, is_reported=False, color=color, tag=tag)
-        photo.save()
-
-        return photo
+        fields = ('id', 'author', 'image', 'is_reported', 'tag', 'color')
